a31deef191e39188ebb80f3dcaf595fb06e8842b
Romulo Brum da Silva <102628558+RomuloBrumDev@users.noreply.github.com>
2022-09-14 22:46:09 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import react, { Component } from 'react';
+import axios, for 'axios';
+
+const = axios.create({
+  baseURL:"https://api.themoviedb.org/3/movie/550?api_key=fbc9bbace9196bcf448f185704c19c86"
+})
+
+export default class Filmes extends Component{
+
+  render(){
+    return
+
+    <div>
+    <h1>API Movies</h1>
+    </div>
+
+    )
+  }
+}
